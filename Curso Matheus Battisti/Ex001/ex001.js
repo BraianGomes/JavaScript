37733aa5c2d4f67bcf707c7225af7a8afe4739f3
Braian de Carvalho Gomes <gomes.braian88@gmail.com>
2023-08-03 09:52:05 +1000

    Update ex001.js

--- Curso Matheus Battisti/Ex001/ex001.js
+++ Curso Matheus Battisti/Ex001/ex001.js
@@ -2,3 +2,10 @@
 //Criar um script que imprime "Ola, Mundo!"
 
 console.log("Ola, Mundo!");
+
+const valorString = "1234"
+const valorNumber = Number(valorString)
+
+console.log(typeof valorNumber);
+
+
