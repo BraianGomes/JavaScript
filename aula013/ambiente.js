5e7142c24aaeb7f3e227a312eb4d9363b5d03e7d
Braian de Carvalho Gomes <gomes.braian88@gmail.com>
2023-08-18 22:09:02 +1000

    Create ambiente.js

---
+++ aula013/ambiente.js
@@ -0,0 +1,14 @@
+/*
+console.log('Tudo bem!');
+console.log('Tudo bem!');
+console.log('Tudo bem!');
+console.log('Tudo bem!');
+console.log('Tudo bem!');
+console.log('Tudo bem!');
+*/
+
+var contador = 1;
+while (contador <= 2){
+    console.log(`Passa ${contador}`);
+    contador ++
+}
