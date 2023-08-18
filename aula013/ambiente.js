c069236926e68d709a826546618911a6f7a4f11b
Braian de Carvalho Gomes <gomes.braian88@gmail.com>
2023-08-18 22:16:14 +1000

    Update ambiente.js

--- aula013/ambiente.js
+++ aula013/ambiente.js
@@ -7,8 +7,18 @@ console.log('Tudo bem!');
 console.log('Tudo bem!');
 */
 
+/*
 var contador = 1;
 while (contador <= 2){
     console.log(`Passa ${contador}`);
     contador ++
 }
+*/
+
+var contador = 1;
+do {
+    console.log(`Passo ${contador}`);
+    contador ++
+} while (contador <=7);
+
+
