4426494fc792261e2aa69066f56b075898e2c1e1
Braian de Carvalho Gomes <gomes.braian88@gmail.com>
2023-08-20 21:48:09 +1000

    Update vetornatela.js

--- aula015/vetornatela.js
+++ aula015/vetornatela.js
@@ -1,3 +1,11 @@
 let valores = [8,1,7,4,2,9]
 
+
+
+/*
 console.log(valores[0])
+*/
+
+for(let pos=0; pos < valores.length; pos++){
+    console.log(`A posicao ${pos} tem o valor ${valores[pos]}`)
+}
