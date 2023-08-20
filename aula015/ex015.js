98e09ef8443fb6684636c6d363dd72b2797fb0cc
Braian de Carvalho Gomes <gomes.braian88@gmail.com>
2023-08-20 22:02:20 +1000

    Update ex015.js

--- aula015/ex015.js
+++ aula015/ex015.js
@@ -6,3 +6,10 @@ numero.sort()
 console.log(`Nosso vetor tem ${numero.length} posicoes`)
 console.log(numero.sort())
 console.log(`O primeiro valor de vetor eh ${numero[0]}`)
+
+let pos = numero.indexOf(0)
+if (pos ==-1){
+    console.log('O valor nao foi encontado')
+} else {
+    console.log(`O valor 11 esta na posicao ${pos}`)
+}
