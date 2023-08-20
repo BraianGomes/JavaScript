51bea440ed8dd3c273962dab24318189d177bfda
Braian de Carvalho Gomes <gomes.braian88@gmail.com>
2023-08-20 21:45:18 +1000

    updating

--- aula015/ex015.js
+++ aula015/ex015.js
@@ -1,5 +1,8 @@
 let numero = [5,7,2,9,11,66]
 numero[6] = 112
 numero.push(33)
+numero.sort()
 
-console.log(`Nosso vetor eh o ${numero}`)
+console.log(`Nosso vetor tem ${numero.length} posicoes`)
+console.log(numero.sort())
+console.log(`O primeiro valor de vetor eh ${numero[0]}`)
