a25e80b5f840b9ff3d2e12eb1396794ec41a3d13
Braian de Carvalho Gomes <gomes.braian88@gmail.com>
2023-08-21 23:13:30 +1000

    updating

--- aula018/ex001/script.js
+++ aula018/ex001/script.js
@@ -1,1 +1,24 @@
-alert('Ola!')
+function loading(){
+    var message = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = `Agora sao ${hora} horas`
+
+
+    if (hora >= 0 && hora < 12){
+        img.src = ('images/morning.jpg')
+        document.body.style.background = '#e2cd9f'
+
+    } else if (hora >= 12 && hora <=18){
+        img.src = ('images/evening.jpg')
+        document.body.style.background = '#b9846f'
+
+    } else {
+        img.src = ('images/night.jpg')
+        document.body.style.background = '#515154'
+    }
+
+    
+}
