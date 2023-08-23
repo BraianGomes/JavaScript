5cd16b946fcf3824bab83bdfc33ca4aa47bd830d
Braian de Carvalho Gomes <gomes.braian88@gmail.com>
2023-08-23 10:59:38 +1000

    updating

--- aula018/AnalisadorDeNumeros/script.js
+++ aula018/AnalisadorDeNumeros/script.js
@@ -1,1 +1,29 @@
+let num = document.querySelector('input#fnum')
+let flista = document.querySelector('select#flista')
+let resultado = document.querySelector('div#resultado')
+let valores = []
 
+function analisarNumero(n) {
+    if (Number(n) >=1 && Number(n) <=100){
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    } else {
+        return false
+    }
+}
+
+
+function adicionar(){
+    if (analisarNumero(num.value) && !inLista(num.value,valores)){
+
+    } else {
+        window.alert('Valor invalido ou ja encontrado na lista!')
+    }
+}
