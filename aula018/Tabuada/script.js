01d6e4224d92407cd3c1011aa161985d61f511d0
Braian de Carvalho Gomes <gomes.braian88@gmail.com>
2023-08-23 10:19:25 +1000

    create

--- aula018/Tabuada/script.js
+++ aula018/Tabuada/script.js
@@ -8,8 +8,14 @@ function tabuada() {
 } else {
     let n = Number(num.value)
     let contador = 1
+    tab.innerHTML = ''
+
     while (contador <= 10){
-        let item = document.creat
+        let item = document.createElement('option')
+        item.text = `${n} x ${contador} = ${n*contador}`
+        item.value = `tab${contador}`
+        tab.appendChild(item)
+        contador++
     }
 }
 
